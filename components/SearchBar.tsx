f4e07ff17ef7c25c9b6aeb8a80c76496767ac5c3
howardhokyin <howard.hyca@gmail.com>
2024-01-07 13:13:52 -0700

    Completed Car serachbar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { SearchManufacturer } from "./";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+
+  const handleSearch = () => {};
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__items">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
-import { MouseEventHandler } from "react";
+import React, { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: React.Dispatch<React.SetStateAction<string>>;
 }
